de84da7306471e2551f52138fc12b1572cdb8672
root <root@DESKTOP-5CO6E6Q.localdomain>
2025-07-16 10:53:10 -0300

    added user field to todo model and to the test creating todo

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Todo(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+   
 
     def __str__(self):
         return self.title
--- todo/tests/test_todo_model.py
+++ todo/tests/test_todo_model.py
@@ -1,13 +1,18 @@
 import pytest
-from todo.models import Todo
+from todo.models import Todo, User
+
 
 @pytest.mark.django_db
 def test_creating_todo():
-    obj = Todo.objects.create(title="Watching SuperMan", description="go to theater watch the new superman movie", is_done=False,)
+    user = User.objects.create(username="cheng yi", password="1234")
+    obj = Todo.objects.create(user=user,title="Watching SuperMan", description="go to theater watch the new superman movie", is_done=False)
+    assert user.username == "cheng yi"
+    assert user.password == "1234"
     assert obj.title == "Watching SuperMan"
     assert obj.description == "go to theater watch the new superman movie"
     assert obj.is_done == False
     assert Todo.objects.count() == 1
+
 
 
 
