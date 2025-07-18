54a719d4abfac51e3563241db854a06a85799b61
root <root@DESKTOP-5CO6E6Q.localdomain>
2025-07-18 15:48:18 -0300

    added search filter for tasks

--- todo/viewsets.py
+++ todo/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import filters
 from todo.serializers import UsersSerializer, TodoSerializer
 from todo.models import User, Todo
 
@@ -24,6 +25,8 @@ class TodoViewSet(viewsets.ModelViewSet):
     http_method_names = ('post', 'get', 'put', 'delete')    
     permission_classes = [IsAuthenticated]
     serializer_class = TodoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['task']
 
     def get_queryset(self):
         return Todo.objects.all()
--- todoproject/settings.py
+++ todoproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'todo',    
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     
 ]
 
@@ -138,6 +139,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
         'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
